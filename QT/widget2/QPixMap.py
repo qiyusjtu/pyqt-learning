cb2b0f3aff3dc10695798a165a5de9e267ad9d11
YuQi <qiyu_sjtu@163.com>
2017-03-12 15:07:28 +0800

    update pixMap

--- QT/widget2/QPixMap.py
+++ QT/widget2/QPixMap.py
@@ -2,3 +2,34 @@
 # -*- coding: utf-8 -*-
 # YuQi created
 __mtime__ = '2017/3/12'
+
+import sys
+from PyQt4 import QtGui,QtCore
+
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        hbox = QtGui.QHBoxLayout(self)
+        pixmap = QtGui.QPixmap('../icons/music.png')
+        label1 = QtGui.QLabel(self)
+        label1.setPixmap(pixmap)
+
+        hbox.addWidget(label1)
+        self.setLayout(hbox)
+
+        self.setWindowTitle('Haha')
+        self.resize(250,300)
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
+
+if __name__ =='__main__':
+    main()
